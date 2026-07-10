04b0e31f5ef7c97cbcc8b10a419b4a7e3f92eee4
Aman Yadav <amanyadav@Amans-MacBook-Air.local>
2026-07-11 01:27:35 +0530

    while and for loop

--- index.js
+++ index.js
@@ -42,12 +42,80 @@ for(let i = 0; i < arr.length;i++){
 }
 
 console.log("---------------------------------------------------");
+/*
+    That's a great question. The answer is simple:
+
+    Use a for loop when you know how many times you need to repeat something.
+    Use a while loop when you don't know how many times it will repeat, and you want to continue until a condition becomes false.
+
+    Let's look at real-world examples.
+*/
+
+/*
+    1. for loop (Most Common)
+
+    Use a for loop when working with arrays or when the number of iterations is known.
+
+    Example 1: Display products
+*/
+
+const products = ["Laptop","Phone","Watch"];
+
+for(let i = 0; i < products.length;i++){
+    console.log(products[i])
+};
+/*
+    Real-world: An e-commerce website displaying products.
+*/
+
+console.log("---------------------------------------------------");
+
+/*
+    Example 2: Send email to every employee
+*/
+const employees = ["Alice", "Bob", "Charlie"];
+
+for(let i = 0; i < employees.length;i++){
+    console.log(`Sending the mail ${employees[i]}`);
+}
+
+console.log("---------------------------------------------------");
+/*
+    Example 3: Create 10 stars
+*/
+
+for(let i = 0; i <=10;i++){
+    console.log("*");
+}
+
+console.log("---------------------------------------------------");
+
+/*
+    Example 4: Calculate total price
+*/
+
+const prices = [100, 200, 300];
+let total = 0;
+
+for(let i = 0; i < prices.length ;i++){
+    total += prices[i];
+}
+console.log(total)
+
+console.log("---------------------------------------------------");
 
 /*
     While Loop 
 */
-let x = 0;
-while(x < 5){
-    console.log(x);
-    x++;
-}
+console.log(`
+
+2. while loop
+Use while when you don't know how many times the loop will run.
+
+    `)
+
+/*
+    Example 1: Login system
+*/
+
+
